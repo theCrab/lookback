0a42764d8ea3bf9ee6c9f71fcbcec8e04914fd94
Minku Lee <premist@me.com>
2016-06-11 19:42:04 +0900

    Use MapBox from Storyboard
    ...instead appending it to the view programmatically.

---
+++ lookback/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  lookback
+//
+//  Created by Premist on 6/6/16.
+//  Copyright © 2016 Premist. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MainViewController: UIViewController,
+                      MGLMapViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
