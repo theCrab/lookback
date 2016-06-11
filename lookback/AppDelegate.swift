36a9c2a09415b69ceb3e11048ee931aa7034409e
Minku Lee <premist@me.com>
2016-06-11 20:33:12 +0900

    Add MGLMapboxAccessToken from xcconfig value

--- lookback/AppDelegate.swift
+++ lookback/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Mapbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
         return true
     }
 
