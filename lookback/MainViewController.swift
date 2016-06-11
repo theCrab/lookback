63527f0e662ad4af8dfdd8ac169082930978a945
Minku Lee <premist@me.com>
2016-06-11 21:32:22 +0900

    Draw coordinates on map from Firebase

--- lookback/MainViewController.swift
+++ lookback/MainViewController.swift
@@ -14,15 +14,38 @@ import FirebaseDatabase
 class MainViewController: UIViewController,
                       MGLMapViewDelegate {
     
+    // MARK: Properties
     var ref: FIRDatabaseReference!
+    @IBOutlet weak var mapView: MGLMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ref = FIRDatabase.database().reference()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        drawCoordinates()
     }
     
+    // Draw coordinates from Firebase database
+    func drawCoordinates() {
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_async(queue, {
+            self.ref.child("coordinates").observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+                let coordsDict = snapshot.value as! [String: AnyObject]
+                
+                for (_, coord) in coordsDict {
+                    let coordDict = coord as! Dictionary<String, Double>
+                    
+                    let marker = MGLPointAnnotation()
+                    marker.coordinate = CLLocationCoordinate2D(latitude: coordDict["latitude"]!, longitude: coordDict["longitude"]!)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        [unowned self] in self.mapView.addAnnotation(marker)
+                    })
+                }
+            })
+        })
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
