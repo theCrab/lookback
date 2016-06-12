1090b0e3b81061f0abbd5985c03da24638f2e28d
Minku Lee <premist@me.com>
2016-06-12 15:34:07 +0900

    Display all Journey in JourneysViewController

--- lookback/JourneysViewController.swift
+++ lookback/JourneysViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class JourneysViewController: UITableViewController {
-
+    var journeys: Results<Journey>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        journeys = Journey.all()
+        print(journeys)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -33,23 +38,25 @@ class JourneysViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return journeys!.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("JourneyCell", forIndexPath: indexPath)
 
+        let journey = journeys![indexPath.row] as Journey
+        
+        cell.textLabel?.text = journey.name
+        cell.detailTextLabel?.text = "0 Coordinates"
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
